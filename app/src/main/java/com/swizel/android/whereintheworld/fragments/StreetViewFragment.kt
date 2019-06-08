5e226877e284f6755f7339a6958b6ad4d37b7bf3
Andrew Kelly <andy@andrews-mbp.lan>
2019-06-08 23:45:52 +1000

    Show basic StreetView

--- app/src/main/java/com/swizel/android/whereintheworld/fragments/StreetViewFragment.kt
+++ app/src/main/java/com/swizel/android/whereintheworld/fragments/StreetViewFragment.kt
@@ -1,22 +1,23 @@
 package com.swizel.android.whereintheworld.fragments
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.StreetViewPanorama
-import com.google.android.gms.maps.StreetViewPanoramaFragment
+import com.google.android.gms.maps.StreetViewPanoramaOptions
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment
+import com.google.android.gms.maps.model.LatLng
 import com.swizel.android.whereintheworld.R
-import dagger.android.support.DaggerFragment
 
 
 
 class StreetViewFragment : Fragment() {
 
-    private val mStreetViewFragment: StreetViewPanoramaFragment? = null
-
-    private val mStreetViewPanorama: StreetViewPanorama? = null
+    private lateinit var streetViewFragment: SupportStreetViewPanoramaFragment
+    private lateinit var streetViewPanorama: StreetViewPanorama
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_streetview, container, false)
@@ -24,5 +25,37 @@ class StreetViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val options = StreetViewPanoramaOptions().apply {
+            streetNamesEnabled(false)
+            position(LatLng(-33.8688,151.2093), 500) // FIXME: Sydney
+        }
+
+        streetViewFragment = SupportStreetViewPanoramaFragment.newInstance(options).apply {
+            getStreetViewPanoramaAsync { panorama ->
+                System.out.println("getStreetViewPanoramaAsync")
+                //            roundNumber.setText(GameState.getInstance().getCurrentRound() + 1 + "/" + Config.MAX_ROUNDS)
+
+                streetViewPanorama = panorama.apply {
+                    setOnStreetViewPanoramaChangeListener { streetViewPanoramaLocation ->
+                        if (streetViewPanoramaLocation != null) {
+                            System.out.println("streetViewPanoramaLocation : $streetViewPanoramaLocation")
+                        }
+                    }
+                }
+            }
+        }
+
+
+        val ft = fragmentManager!!.beginTransaction()
+        ft.replace(R.id.streetViewStub, streetViewFragment)
+        ft.commit()
+    }
+
+    private fun toggleStreetViewEnabled(enabled: Boolean) {
+        streetViewPanorama.isPanningGesturesEnabled = enabled
+        streetViewPanorama.isUserNavigationEnabled = enabled
+        streetViewPanorama.isZoomGesturesEnabled = enabled
+//        shroud.setVisibility(if (!enabled) View.VISIBLE else View.GONE)
     }
 }
