a2d4a006bd1aa60076a088600f392a645cd35535
Andrew Kelly <andy@andrews-mbp.lan>
2019-06-11 23:52:35 +1000

    Add Google Map with pin dropping mechanic.

---
+++ app/src/main/java/com/swizel/android/whereintheworld/Config.kt
@@ -0,0 +1,13 @@
+package com.swizel.android.whereintheworld
+
+object Config {
+    const val MAP_GUESS_H_ANCHOR = 0.16f
+
+    const val MAP_GUESS_V_ANCHOR = 0.81f
+
+    const val MAP_INFO_H_ANCHOR = 0.5f
+
+    const val MAP_INFO_V_ANCHOR = 0.14f
+
+    const val MAPPING_DROPPED_PIN_ANIMATION_SPEED_MS = 750L
+}
